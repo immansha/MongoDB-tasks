05cd18d1d9084a4fd859e2a88126fc037ae02ec2
Mansha Kshatriya <manshakshatriya2022@vitbhopal.ac.in>
2025-04-02 23:02:11 +0530

    Update 14-02-25.sql

--- 14-02-25.sql
+++ 14-02-25.sql
@@ -3,7 +3,7 @@ use friday;
 show tables from friday;
 
 create table student(id int, name varchar(20), location varchar(20));
-insert into student values (101, 'Shubham','Samastipur'),(102,'Joy','Kolkata');
+insert into student values (101, 'Mansha','Gujarat'),(102,'Sanjana','Surat');
 
 select *from student;
 
